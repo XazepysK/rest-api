b4eb8ab12fed8d0a0eeb109646d99140107f2a59
XazepysK <minikrap2@gmail.com>
2026-09-03 23:10:52 +0700

    XazepysK

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.set("json spaces", 2);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   const og = res.json;
   res.json = function (data) {
