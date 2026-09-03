f5d52baf6c48fc17def395cdd1d69fb34b6d11fa
XazepysK <minikrap2@gmail.com>
2026-09-03 22:36:29 +0700

    XazepysK

--- index.js
+++ index.js
@@ -37,6 +37,9 @@ fs.readdirSync(apiFolder).forEach((subfolder) => {
     });
   }
 });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 app.use((req, res, next) => {
     res.status(404).json({
       message: "404 - not found"
